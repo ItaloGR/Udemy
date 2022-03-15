5f385632ae49c60eb137ad93c12570f3a3788f38
Italo Gonçalves <italo.goncalves@outlook.com>
2022-03-14 22:28:34 -0300

    comit 0123

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Frase } from 'src/shared/frases.model';
+import { FRASES } from './frases.mock';
 
 @Component({
   selector: 'app-painel',
@@ -7,9 +9,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PainelComponent implements OnInit {
 
-  constructor() { }
+  public frases: Frase[] = FRASES;
+  public instrucao: string = 'Traduza a fraze:';
+  public resposta: string = '';
+
+  public rodada: number = 0;
+  public rodadaFrase: Frase;
+
+
+  public progresso: number = 0;
+  public progressoTotal: number = 0;
+
+  public tentativasTotal: number;
+  public tentativasRestantes: number;
+
+  public totalPerguntas: number;
+  constructor() { 
+    this.totalPerguntas = this.frases.length
+    this.rodadaFrase = this.frases[this.rodada];
+    this.progresso = 100 / this.totalPerguntas;
+
+    
+    this.tentativasTotal = this.totalPerguntas
+    this.tentativasRestantes = this.totalPerguntas
+  }
 
   ngOnInit(): void {
   }
 
+  atualizaResposta(input: Event): void{
+    this.resposta = (input.target as HTMLInputElement).value
+    this.resposta = (<HTMLInputElement>input.target).value
+  }
+
+  verificaResposta(): void{
+    if(this.rodadaFrase.frasePtbr.toUpperCase() === this.resposta.toUpperCase())
+    {
+      if(this.totalPerguntas = this.rodada)
+      {
+        alert('Very good!');
+        return;
+      }
+      this.rodada++;
+      this.rodadaFrase = this.frases[this.rodada];
+      this.progressoTotal += this.progresso;
+      alert('Tradução correta')
+    }else{
+      this.tentativasRestantes--;
+      if(this.tentativasRestantes == 0)
+      {
+        alert('Fim de jogo!');
+        window.location.reload();
+      }
+
+      alert('Tradução errada')
+    }
+  }
 }
--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-tentativas',
   templateUrl: './tentativas.component.html',
   styleUrls: ['./tentativas.component.css']
 })
-export class TentativasComponent implements OnInit {
+export class TentativasComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  coracaoCheio: string = "/assets/coracao_cheio.png";
+  coracaoVazio: string = "/assets/coracao_vazio.png";
+
+  chances: string[];
+
+  @Input() public total: number = 0;
+  @Input() public restante: number = 0;
+
+  constructor() {
+    this.chances = []
+  }
 
   ngOnInit(): void {
+    for(let i = 0; i < this.total; i++){
+      this.chances.push(this.coracaoCheio)
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes["restante"].previousValue != this.restante)
+      this.chances.map((_value: string, index: number)=>{
+          if((this.restante) < index+1)
+          this.chances[index] = this.coracaoVazio;
+        }
+    )
   }
 
 }
